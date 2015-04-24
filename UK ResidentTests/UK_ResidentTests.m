3b753a9d528ec704ab5856a5dc56472e258f21c4
Sergey Shvedov <sergey.shvedov@saprun.com>
2015-04-24 12:56:40 +0300

    Initial Commit

---
+++ UK ResidentTests/UK_ResidentTests.m
@@ -0,0 +1,40 @@
+//
+//  UK_ResidentTests.m
+//  UK ResidentTests
+//
+//  Created by Sergey Shvedov on 24.04.15.
+//  Copyright (c) 2015 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface UK_ResidentTests : XCTestCase
+
+@end
+
+@implementation UK_ResidentTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
